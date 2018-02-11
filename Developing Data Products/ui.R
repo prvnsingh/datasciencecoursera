3e7408ccc1b315dda5e3fe255403b82114d2f41e
prvnsingh <prvns1997@gmail.com>
2018-02-12 00:17:35 +0530

    Developing Data Products
    Documentation added to ui.R

--- Developing Data Products/ui.R
+++ Developing Data Products/ui.R
@@ -1,19 +1,24 @@
 library(shiny)
 shinyUI(fluidPage(
   
-  titlePanel("Shiny Application and Reproducible Pitch"),
+  titlePanel("Shiny Application-Predicting Solar Radiation for given Month and Temperature"),
   
   sidebarLayout(
     sidebarPanel(
       radioButtons("mon","Select the month",c("MAY","JUNE","JULY","AUGUST","SEPTEMBER"), selected = "MAY"),
-      textInput("temp","Enter the temperature(50-100)")
+      textInput("temp","Enter the temperature(50-100)"),
+      h4("Predicted Solar Radiation"),
+      textOutput("prediction")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("Plot1"),
-      h3("Predicting Solar Radiation for given Month and Temperature"),
-      textOutput("prediction")
+      h3("Airquality data set of R is used in making this application.
+         The graph depicts the variation of solar radiation with respect of temperature for selected Month.
+         The line depicts the Linear Regression Model fitted to predict the solar radiation for particular temperature.
+         Select the months from options and enter the Temperature and see the forecast"),
+      plotOutput("Plot1")
+      
     )
   )
 ))
